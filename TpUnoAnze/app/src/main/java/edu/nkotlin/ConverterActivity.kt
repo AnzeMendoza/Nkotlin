ec5ba4b662f9297383d5262763b103f653c6cdbf
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2021-06-29 14:45:09 -0300

    update

--- TpUnoAnze/app/src/main/java/edu/nkotlin/ConverterActivity.kt
+++ TpUnoAnze/app/src/main/java/edu/nkotlin/ConverterActivity.kt
@@ -2,10 +2,29 @@ package edu.nkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 
 class ConverterActivity : AppCompatActivity() {
+
+    lateinit var temperatura: EditText
+    lateinit var convFtoC: CheckBox
+    lateinit var convCtoF: CheckBox
+    lateinit var converte: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_converter)
+        initializeElements()
+
+        
+    }
+
+    fun initializeElements(){
+        temperatura = findViewById(R.id.e_converter_value)
+        convCtoF = findViewById(R.id.cb_converter_cToF)
+        convFtoC = findViewById(R.id.cb_converter_fToC)
+        converte = findViewById(R.id.b_converter_converter)
     }
 }
