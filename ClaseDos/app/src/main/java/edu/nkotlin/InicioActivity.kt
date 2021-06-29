19ee379adde4fa69c9002a3fa5e1cade6bdfe0da
Sucho <anze.mendoza@gmail.com>
2021-06-29 00:13:55 -0300

    se agregon los 3 layout

---
+++ ClaseDos/app/src/main/java/edu/nkotlin/InicioActivity.kt
@@ -0,0 +1,11 @@
+package edu.nkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
