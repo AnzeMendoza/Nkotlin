88141cec1e2eb7ffafe64cc5affe2108367c3990
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2021-06-30 17:48:32 -0300

    listas mutables e inmutables

--- Poo/src/main/kotlin/main.kt
+++ Poo/src/main/kotlin/main.kt
@@ -1,14 +1,24 @@
 
 fun main(args: Array<String>) {
-    val movie = Movie("matrix 2", 2)
-    val bb = Series("Breaking bad", 2)
-    bb.totalEpisodes = 2
-    // me permite objetos de las clases hijas
-    val list = mutableListOf<Media>()
-    list.add(movie)
-    list.add(bb)
+    val matrix = Movie("Matrix", 2)
+    val cars = Movie("Cars", 3)
+    val coco = Movie("Coco", 1)
+    val toyStory = Movie("Toy story", 2)
 
-    list[0].play()
-    list[1].play()
+    val breakingBad = Series("Breaking Bad", 1)
+    val invincible = Series("Invincible", 2)
+    val dark = Series("Dark", 3)
+    val tbbt = Series("The big ban theory", 2)
+    // lista normal
+    val movieList = listOf(matrix, cars, coco, toyStory)
 
+    // lista mutable - podemos modificarla a diferencia de las anteriores
+    val seriesList = mutableListOf(breakingBad, invincible)
+    seriesList.add(dark)
+    seriesList[0] = tbbt
+
+    /*
+    La elección de cada uno depende la funiconalidad, como regla si no se modifica usar los in
+    mutables.
+    */
 }
