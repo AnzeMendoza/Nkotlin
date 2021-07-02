0e76acb2031ee6318df39be1122055f598c7ec62
Sucho <anze.mendoza@gmail.com>
2021-07-02 16:55:32 -0300

    se agrego la funcionalidad de tomar fotos

--- RegistroDeSuperheroes/app/src/main/java/edu/nkotlin/DetailActivity.kt
+++ RegistroDeSuperheroes/app/src/main/java/edu/nkotlin/DetailActivity.kt
@@ -1,5 +1,6 @@
 package edu.nkotlin
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.nkotlin.databinding.ActivityDetailBinding
@@ -7,7 +8,7 @@ import edu.nkotlin.databinding.ActivityDetailBinding
 class DetailActivity : AppCompatActivity() {
     companion object {
         const val SUPERHERO_KEY = "superhero"
-
+        const val BITMAP_KEY = "bitmap"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,9 @@ class DetailActivity : AppCompatActivity() {
         val bundle = intent.extras!!
 
         val superhero = bundle.getParcelable<Superhero>(SUPERHERO_KEY)!!
+        val heroImage = bundle.getParcelable<Bitmap>(BITMAP_KEY)!!
+
+        binding.ivHero.setImageBitmap(heroImage)
         binding.superhero = superhero
     }
 }
--- RegistroDeSuperheroes/app/src/main/java/edu/nkotlin/MainActivity.kt
+++ RegistroDeSuperheroes/app/src/main/java/edu/nkotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.nkotlin
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
 import edu.nkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var heroImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,11 +27,32 @@ class MainActivity : AppCompatActivity() {
             val hero = Superhero(superHeroName, alterEgo, bio, ratingPower)
             openDetailActivity(hero)
         }
+        heroImage = binding.ivHero
+        heroImage.setOnClickListener{
+            openCamera()
+        }
     }
+
+    private fun openCamera() {
+        // es un intent implicito solo tiene un origen.
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, 1000)
+    }
+
     private fun openDetailActivity(superhero: Superhero){
+        // es un intent explicito, tiene origen y destino
         val intent = Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.SUPERHERO_KEY, superhero)
-
+        intent.putExtra(DetailActivity.BITMAP_KEY, heroImage.drawable.toBitmap())
         startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 1000){
+            val extras = data?.extras
+            val heroMap  = extras?.getParcelable<Bitmap>("data")
+            heroImage.setImageBitmap(heroMap)
+        }
     }
 }
