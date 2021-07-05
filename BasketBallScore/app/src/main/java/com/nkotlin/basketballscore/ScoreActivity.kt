0d35519a33f7576bcb65fd290b8d15456b7dcef8
hanzemendoza <hanzemendoza@est.frba.utn.edu.ar>
2021-07-05 17:01:15 -0300

    se agregaron imagenes y se terminaron lo layout

--- BasketBallScore/app/src/main/java/com/nkotlin/basketballscore/ScoreActivity.kt
+++ BasketBallScore/app/src/main/java/com/nkotlin/basketballscore/ScoreActivity.kt
@@ -2,12 +2,10 @@ package com.nkotlin.basketballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.nkotlin.basketballscore.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class ScoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_score)
     }
 }
